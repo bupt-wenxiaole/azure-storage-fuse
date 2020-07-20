6afc45a7af5c927e756c65320db803dec53c93c7
vibhansa-msft <vibhansa@microsoft.com>
2020-07-20 20:29:04 +0530

    Remvoing curl options as it degraded the performance

--- cpplite/include/http/libcurl_http_client.h
+++ cpplite/include/http/libcurl_http_client.h
@@ -348,8 +348,6 @@ namespace azure {  namespace storage_lite {
             curl_global_init(CURL_GLOBAL_DEFAULT);
             for (int i = 0; i < m_size; i++) {
                 CURL *h = curl_easy_init();
-                curl_easy_setopt(h, CURLOPT_MAXCONNECTS, 50L);
-                curl_easy_setopt(h, CURLOPT_PIPEWAIT, 1L);
                 m_handles.push(h);
             }
         }
@@ -361,8 +359,6 @@ namespace azure {  namespace storage_lite {
             for (int i = 0; i < m_size; i++) {
                 CURL *h = curl_easy_init();
                 curl_easy_setopt(h, CURLOPT_CAPATH, ca_path.c_str());
-                curl_easy_setopt(h, CURLOPT_MAXCONNECTS, 50L);
-                curl_easy_setopt(h, CURLOPT_PIPEWAIT, 1L);
                 m_handles.push(h);
             }
         }
